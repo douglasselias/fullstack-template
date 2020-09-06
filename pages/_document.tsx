babc9dbb3b4ee144d68bf029629c5fdb68f7fbce
douglas <douglassantoselias@gmail.com>
2020-09-06 19:20:35 -0300

    Add type for document

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,35 +1,21 @@
-// import Document, { Html, Head, Main, NextScript } from 'next/document'
+import Document, { Html, Head, Main, NextScript } from 'next/document'
 
-// export default class MyDocument extends Document {
-//   render(): Element {
-//     return (
-//       <Html lang="pt-BR">
-//         <Head>
-//           <meta charSet="utf-8" />
-//           <meta name="description" content="Fullstack Template" />
-//           <meta name="theme-color" content="#2ebd59" />
+export default class MyDocument extends Document {
+  render(): JSX.Element {
+    return (
+      <Html lang="pt-BR">
+        <Head>
+          <meta charSet="utf-8" />
+          <meta name="description" content="Fullstack Template" />
+          <meta name="theme-color" content="#2ebd59" />
 
-//           <link rel="shortcut icon" href="favicon.png" />
-//         </Head>
-//         <body>
-//           <Main />
-//           <NextScript />
-//         </body>
-//       </Html>
-//     )
-//   }
-// }
-
-import Document, { DocumentContext, DocumentInitialProps } from 'next/document'
-
-class MyDocument extends Document {
-  static async getInitialProps(
-    ctx: DocumentContext
-  ): Promise<DocumentInitialProps> {
-    const initialProps = await Document.getInitialProps(ctx)
-
-    return initialProps
+          <link rel="shortcut icon" href="favicon.png" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
   }
 }
-
-export default MyDocument
