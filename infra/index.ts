bcda4ea5f4db17a9c128b9ef0e33d2413b75c11f
douglasselias <douglassantoselias@gmail.com>
2021-01-16 11:46:43 -0300

    fix: wow lint staged

--- infra/index.ts
+++
@@ -1,2 +0,0 @@
-export * from './repository-add-task'
-export * from './repository-list-tasks'
--- use-cases/add-task/add-task.presentation.tsx
+++ use-cases/add-task/add-task.presentation.tsx
@@ -1,26 +1,25 @@
-// import { useRef } from 'react'
+import { useRef } from 'react'
 
-// import { AddTaskUseCase } from './add-task.usecase'
+import { AddTaskUseCase } from './add-task.usecase'
 
-// export function AddTaskComponent(props: Props) {
-//   const { addTask, callback } = props
-//   const inputTask = useRef<HTMLInputElement>()
+export function AddTaskPresentation(props: Props) {
+  const { addTaskUseCase, callback } = props
+  const inputTask = useRef<HTMLInputElement>()
 
-//   function handleAddTask() {
-//     addTask.add(inputTask.current.value)
-//     callback()
-//   }
+  async function handleAddTask() {
+    await addTaskUseCase.add(inputTask.current.value)
+    callback()
+  }
 
-//   return (
-//     <>
-//       <input ref={inputTask} placeholder="Digite uma tarefa" />
-//       <button onClick={handleAddTask}>Adicionar</button>
-//     </>
-//   )
-// }
+  return (
+    <>
+      <input ref={inputTask} placeholder="Digite uma tarefa" />
+      <button onClick={handleAddTask}>Adicionar</button>
+    </>
+  )
+}
 
-// type Props = {
-//   addTask: AddTaskUseCase
-//   callback(): void
-// }
-export {}
+type Props = {
+  addTaskUseCase: AddTaskUseCase
+  callback(): void
+}
