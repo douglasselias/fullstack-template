3e29be9bb8a0aeea01b1ab5e6be04b71f6422541
douglasselias <douglassantoselias@gmail.com>
2021-06-17 23:49:28 -0300

    feat: add chakra ui

---
+++ pages/_app.tsx
@@ -0,0 +1,16 @@
+import { AppProps } from 'next/app'
+
+import { ChakraProvider, Flex } from '@chakra-ui/react'
+
+//
+function App({ Component, pageProps }: AppProps) {
+  return (
+    <ChakraProvider>
+      <Flex justify="center">
+        <Component {...pageProps} />
+      </Flex>
+    </ChakraProvider>
+  )
+}
+
+export default App
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,29 @@
+import { Heading } from '@chakra-ui/react'
+import { Input } from '@chakra-ui/react'
+import { Button } from '@chakra-ui/react'
+import { VStack } from '@chakra-ui/react'
+
 function Index() {
-  return <h1>hello world</h1>
+  return (
+    <>
+      <VStack spacing={4} maxWidth="40rem">
+        <Heading>Clean Arch</Heading>
+        <label>
+          E-mail
+          <Input />
+        </label>
+        <label>
+          Senha
+          <Input />
+        </label>
+
+        <Button colorScheme="blue" isFullWidth>
+          Sign In
+        </Button>
+        <Button isFullWidth>Reset Password</Button>
+      </VStack>
+    </>
+  )
 }
 
 export default Index
