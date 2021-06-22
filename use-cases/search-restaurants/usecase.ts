d0f7b1519664f0c325c430661ae3934557cd8a61
douglasselias <douglassantoselias@gmail.com>
2021-06-22 20:17:32 -0300

    feat: create search restaurants use case

---
+++ use-cases/search-restaurants/usecase.ts
@@ -0,0 +1,11 @@
+export type UseCase = (query: string) => Promise<Restaurant[]>
+
+type Restaurant = {
+  imageURL: string
+  name: string
+  category: string
+  rating: number
+  distance: number
+  deliveryTimeRange: [number, number]
+  deliveryTax: number
+}
