6dc38a4ffbb9f789bad31021db2151ec4125f45a
douglasselias <douglassantoselias@gmail.com>
2021-07-05 18:26:54 -0300

    fix: undefined value in localstorage

--- use-cases/list-products/localstorage.test.ts
+++ use-cases/list-products/localstorage.test.ts
@@ -6,15 +6,21 @@ const stubProduct = {
 }
 
 describe('List products - Local Storage', () => {
-  localStorage.setItem(
-    'ALL_PRODUCTS',
-    JSON.stringify({
-      '123-abc': [stubProduct],
-    })
-  )
-
   it('returns a list of products', () => {
+    localStorage.setItem(
+      'ALL_PRODUCTS',
+      JSON.stringify({
+        '123-abc': [stubProduct],
+      })
+    )
+
     const products = storage('123-abc')
     expect(products).toStrictEqual([stubProduct])
+  })
+
+  it('returns an empty list', () => {
+    localStorage.clear()
+    const products = storage('123-abc')
+    expect(products).toStrictEqual([])
   })
 })
